b35b04a0bf9cef5e52874ba2cfbfb707fa639c05
makombe <kennedymakombe@gmail.com>
2024-10-30 09:45:45 +0300

    Add hei outcome encounter endpoint

--- omod/src/main/java/org/openmrs/module/kenyaemr/web/controller/KenyaemrCoreRestController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/web/controller/KenyaemrCoreRestController.java
@@ -85,6 +85,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.xml.sax.SAXException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -573,6 +574,29 @@ public class KenyaemrCoreRestController extends BaseRestController {
         Location facility = Context.getService(KenyaEmrService.class).getLocationByMflCode(facilityCode);
         locationResponseObj.put("name", facility.getName());
         return locationResponseObj;
+    }
+
+    /**
+     * Gets last hei outcome encounter
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/heiOutcomeEncounter")
+    @ResponseBody
+    public Object getHeiOutcomeEncounter(@RequestParam("patientUuid") String patientUuid) {
+        SimpleObject heiOutcomeResponseObj = new SimpleObject();
+        PatientIdentifierType heiNumber = MetadataUtils.existing(PatientIdentifierType.class, MchMetadata._PatientIdentifierType.HEI_ID_NUMBER);
+        Patient patient =  Context.getPatientService().getPatientByUuid(patientUuid);
+        PatientIdentifier pi = heiNumber != null && patient != null ? patient.getPatientIdentifier(heiNumber) : null;
+        EncounterType heiOutcomeEncType = MetadataUtils.existing(EncounterType.class, MchMetadata._EncounterType.MCHCS_HEI_COMPLETION);
+        Form heiOutcomeForm = MetadataUtils.existing(Form.class, MchMetadata._Form.MCHCS_HEI_COMPLETION);
+        Encounter lastHeiOutcomeEnc = EmrUtils.lastEncounter(patient, heiOutcomeEncType, heiOutcomeForm);
+        if(pi.getIdentifier() != null) {
+            heiOutcomeResponseObj.put("heiNumber", pi.getIdentifier());
+        }
+        if(lastHeiOutcomeEnc != null) {
+            heiOutcomeResponseObj.put("heiOutcomeEncounterUuid", lastHeiOutcomeEnc.getUuid());
+        }
+        return heiOutcomeResponseObj;
     }
 
     /**
